ec3564212d48ceb269ada408ad4167d493035fda
John Simmons <johnbonds7000@gmail.com>
2018-02-09 05:23:20 +0000

    Initialization

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-'use strict';
+require('dotenv').config();
 
-var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+const express = require('express');
+const routes = require('./app/routes/index.js');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const bluebird = require('bluebird');
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
 
-mongoose.connect(process.env.MONGO_URI);
-mongoose.Promise = global.Promise;
-
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+mongoose.connect(process.env.MONGO_URI, {
+	useMongoClient: true
+});
+mongoose.Promise = bluebird.Promise;
 
 app.use(session({
-	secret: 'secretClementine',
-	resave: false,
+	secret: 'secretBook',
+	resave: true,
 	saveUninitialized: true
 }));
 
@@ -28,7 +25,7 @@ app.use(passport.session());
 
 routes(app, passport);
 
-var port = process.env.PORT || 8080;
-app.listen(port,  function () {
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
 	console.log('Node.js listening on port ' + port + '...');
 });
